b9d16fa0f98298e791192af4a7cb1d59e987dddb
Munachi <munaonye4@gmail.com>
2026-03-24 19:51:07 +0100

    done with call logs navbar

--- src/Dashboard/CallLogs.tsx
+++ src/Dashboard/CallLogs.tsx
@@ -1,3 +1,5 @@
+import { Bell, Search, UserCircle2 } from "lucide-react";
+
 const CallLogs = () => {
   return (
     <div>
@@ -12,13 +14,29 @@ const CallLogs = () => {
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className="flex items-center gap-4">
+          <div className="flex bg-[#1F2020] items-center gap-2">
+            <Search className="text-white ml-2" size={20} />
             <input
               type="search"
               placeholder="Search Call Logs"
-              className="w-40 py-2 border border-white"
+              className="w-50 py-2.5 bg-[#1F2020] text-[#ACABAA80] font-inter text-sm pl-1 outline-none"
             />
+          </div>
+
+          <div className="font-inter text-right">
+            <p className="text-[#ACABAA] text-sm tracking-wide">
+              AVAILABLE BALANCE
+            </p>
+            <p className="text-[#6BDC9F] font-semibold">₦12,500</p>
+          </div>
+
+          <div className="text-[#ACABAA]">
+            <Bell size={25} />
+          </div>
+
+          <div className="text-[#ACABAA]">
+            <UserCircle2 size={25} />
           </div>
         </div>
       </nav>
