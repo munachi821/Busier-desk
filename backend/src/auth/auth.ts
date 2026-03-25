3e687d553893a6a48de08479fa895309b011efbe
Emmanuel Sunday <135783723+ebubesunday16@users.noreply.github.com>
2026-03-25 19:40:02 +0100

    fix cors bug

--- backend/src/auth/auth.ts
+++ backend/src/auth/auth.ts
@@ -13,4 +13,5 @@ export const auth = betterAuth({
   },
   secret: process.env.BETTER_AUTH_SECRET!,
   baseURL: process.env.BETTER_AUTH_URL ?? 'http://localhost:3000',
+  trustedOrigins: ['http://localhost:5173'],
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,11 @@ async function bootstrap() {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
