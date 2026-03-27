2c827c8602e4eb0b695bf185421e75bf1f00ad63
Emmanuel Sunday <135783723+ebubesunday16@users.noreply.github.com>
2026-03-27 22:38:03 +0100

    fix build

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,7 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client'; 
 import { PrismaPg } from '@prisma/adapter-pg';
 
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
